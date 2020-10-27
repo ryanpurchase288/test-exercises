f45f21f1cd5c286228bae3ce346147e1cf57512f
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-10-27 12:29:39 +0000

    completed

--- tests/factorial_test.py
+++ tests/factorial_test.py
@@ -3,3 +3,11 @@ from applications import factorial
 
 def test_fact():
 	assert factorial.fact(4)==24
+
+def test_fact_1():
+	assert factorial.fact(7)==5040
+
+def test_fact_2():
+	assert factorial.fact(25)==15511210043330985984000000
+
+        
--- tests/vowels_test.py
+++ tests/vowels_test.py
@@ -3,3 +3,12 @@ from applications import vowels
 
 def test_vowel():
 	assert vowels.vowels('Ryan')==1
+
+def test_vowel1():
+	assert vowels.vowels('Lewis')==2
+
+def test_vowel2():
+	assert vowels.vowels('George')==3
+
+def test_vowel3():
+	assert vowels.vowels('Purchase')==3        
